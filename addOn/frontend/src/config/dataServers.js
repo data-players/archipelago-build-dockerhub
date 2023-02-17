efad9f80d6651d494c182b8b59d47d67ddd61e16
simon louvet <simon.louvet.pro@gmail.com>
2023-02-17 20:05:34 +0100

    simplify addon

--- addOn/frontend/src/config/dataServers.js
+++ addOn/frontend/src/config/dataServers.js
@@ -1,13 +1,9 @@
 const dataServers = {
-  av: {
+  myServer: {
     baseUrl: process.env.REACT_APP_MIDDLEWARE_URL,
     authServer: true,
     default: true,
     uploadsContainer: '/files'
-  },
-  cdlt: {
-    baseUrl: 'https://data.lescheminsdelatransition.org/',
-    externalLinks: true,
   }
 };
 
